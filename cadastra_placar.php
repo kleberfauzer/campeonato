f70f08a6f68d9ded26955d2c1260b91943a4796e
Hugo Leonardo <hugo05boss@gmail.com>
2018-06-22 01:55:41 -0300

    Placar + campeonato + cliente

---
+++ cadastra_placar.php
@@ -0,0 +1,8 @@
+<?php
+	include("cabecalho.php");
+	include("funcoes.php");
+
+	cadastra_placar($_POST['id_jogo'], $_POST['time_casa'], $_POST['time_visitante'], $_POST['gols_casa'], $_POST['gols_visitante']);
+	pontuacao_corrida($_POST['id_campeonato']);
+
+?>
